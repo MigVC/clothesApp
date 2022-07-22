4cf928eff8580b4af09337364dec12c3499eedda
MigVC <86692813+MigVC@users.noreply.github.com>
2022-07-21 22:56:18 -0500

    Updates

---
+++ precache-manifest.299499d11839c5a082248b6569ffbd39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bec0d95c89b5c0514b04",
+    "url": "/clothesApp/static/css/main.62714f5d.chunk.css"
+  },
+  {
+    "revision": "bec0d95c89b5c0514b04",
+    "url": "/clothesApp/static/js/main.bec0d95c.chunk.js"
+  },
+  {
+    "revision": "1575f2b38d7b90998e58",
+    "url": "/clothesApp/static/js/1.1575f2b3.chunk.js"
+  },
+  {
+    "revision": "caad86b8db245abd2dda",
+    "url": "/clothesApp/static/js/runtime~main.caad86b8.js"
+  },
+  {
+    "revision": "9b9d25e7a707c8c13371755c6d2c171b",
+    "url": "/clothesApp/static/media/react.9b9d25e7.png"
+  },
+  {
+    "revision": "d4f68bd8aee54092156313fd2cb69514",
+    "url": "/clothesApp/static/media/fakestore.d4f68bd8.png"
+  },
+  {
+    "revision": "893c50cbae2ad41aed913ee26b4abdcb",
+    "url": "/clothesApp/static/media/bootstrap.893c50cb.png"
+  },
+  {
+    "revision": "c167307206eba2e057894c758eda41f8",
+    "url": "/clothesApp/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/clothesApp/precache-manifest.299499d11839c5a082248b6569ffbd39.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/clothesApp/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
